8caf6858051523e80e4bbb8a2292a588219db124
Blurbeast <o.samuel@native.semicolon.africa>
2023-05-29 20:02:48 +0100

    All_In_One

--- src/chichi/stack/ListArrayImplementation.java
+++ src/chichi/stack/ListArrayImplementation.java
@@ -1,6 +1,75 @@
 package chichi.stack;
 
-import java.util.List;
+import java.util.Arrays;
 
-public class ListArrayImplementation implements Lists {
+public class ListArrayImplementation {
+    private boolean isEmpty = true;
+    private int numberOfAdds = 0;
+    private final String[] elements;
+    private final String[] newElements;
+    private int indexOfElement;
+    public ListArrayImplementation (int sizeOfList){
+        elements = new String[sizeOfList];
+        newElements = new String[sizeOfList - 1];
+    }
+    public boolean isEmpty() {
+        return isEmpty;
+    }
+    public void add(String element){
+        this.elements[numberOfAdds] = element;
+        numberOfAdds++;
+        isEmpty = false;
+    }
+    public void add(int index, String element) {
+        this.elements[index] = element;
+        numberOfAdds++;
+        isEmpty = false;
+    }
+    public int size() {
+        return numberOfAdds;
+    }
+    public void remove(int index) {
+        for (int element =0, newElement = 0; element < elements.length ; element++) {
+            if (element != index) {
+                newElements[newElement] =elements[element];
+                newElement++;
+            }
+        }
+        numberOfAdds--;
+    }
+    public void remove(String element){
+        for (int index =0, newElement = 0; index < elements.length ; index++) {
+            if (elements[index] != element) {
+                newElements[newElement] =elements[index];
+                newElement++;
+            }
+        }
+        numberOfAdds--;
+    }
+    public String get(int index) {
+        return this.elements[index];
+    }
+    public void set(int index, String element) {
+        this.elements[index] = element;
+    }
+    public String[] getListElement() {
+        return newElements;
+    }
+    public int indexOf(String element) {
+        String[] newArray;
+        newArray = elements;
+        for (int index = 0; index < newArray.length; index++) {
+            if (newArray[index].equals(element)) return index;
+        }
+        return 0;
+    }
+
+    public boolean contains(String element) {
+        String[] newArray;
+        newArray = elements;
+        for (String array : newArray) {
+            return array.equals(element);
+        }
+        return false;
+    }
 }
---
+++ test/chichi/stack/ListArrayImplementationTest.java
@@ -0,0 +1,135 @@
+package chichi.stack;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ListArrayImplementationTest {
+    ListArrayImplementation listArrayImplementation;
+    @BeforeEach
+    void setUp() {
+        listArrayImplementation = new ListArrayImplementation(5);
+    }
+    @Test public void objectOfClassExist(){
+        assertNotNull(listArrayImplementation);
+    }
+    @Test public void listOfElementsIsEmpty(){
+        assertTrue(listArrayImplementation.isEmpty());
+    }
+    @Test public void listOfElementsIsNotEmptyWhenAnElementISAdded(){
+        listArrayImplementation.add(0, "OOP");
+        assertFalse(listArrayImplementation.isEmpty());
+    }
+    @Test public void elementCanBeAddedTheListWithoutTheIndexAndListNotEmpty(){
+        listArrayImplementation.add("Ope");
+        assertFalse(listArrayImplementation.isEmpty());
+    }
+    @Test public void sizeOFElementIncreaseAfterAdding(){
+        listArrayImplementation.add("Ope");
+        assertFalse(listArrayImplementation.isEmpty());
+        assertEquals(1, listArrayImplementation.size());
+    }
+    @Test public void sizeIncreaseMoreThanOneAfterAddingTwice(){
+        listArrayImplementation.add("Ope");
+        listArrayImplementation.add(0, "OOP");
+        assertFalse(listArrayImplementation.isEmpty());
+        assertEquals(2, listArrayImplementation.size());
+    }
+    @Test public void sizeOfListDecreasesWhenRemovedFromTheLIst(){
+        listArrayImplementation.add(0, "OOP");
+        listArrayImplementation.remove(0);
+        assertFalse(listArrayImplementation.isEmpty());
+        assertEquals(0, listArrayImplementation.size());
+    }
+    @Test public void sizeOfListDecreasesWhenRemoveFromTheListWithTheElement(){
+        listArrayImplementation.add(0, "OOP");
+        listArrayImplementation.add(0, "Tayo");
+        listArrayImplementation.remove("Tayo");
+        assertFalse(listArrayImplementation.isEmpty());
+        assertEquals(1, listArrayImplementation.size());
+    }
+    @Test public void elementInTheListCanBeObtained(){
+        listArrayImplementation.add(0, "OOP");
+        assertFalse(listArrayImplementation.isEmpty());
+        assertEquals("OOP", listArrayImplementation.get(0));
+    }
+    @Test public void elementInTheListCanBeObtainedFromElements(){
+        listArrayImplementation.add(0, "Tayo");
+        listArrayImplementation.add(1, "OOP");
+        assertFalse(listArrayImplementation.isEmpty());
+        assertEquals("Tayo", listArrayImplementation.get(0));
+    }
+    @Test public void elementInTheListCanBeGottenAgain(){
+        listArrayImplementation.add(1, "OOP");
+        listArrayImplementation.add("Dele");
+        listArrayImplementation.add("Ope");
+        assertFalse(listArrayImplementation.isEmpty());
+        assertEquals("Dele", listArrayImplementation.get(1));
+    }
+    @Test public void elementInTheListCanBeUpdated(){
+        listArrayImplementation.add(0, "OOP");
+        listArrayImplementation.add("Dele");
+        listArrayImplementation.add("Ope");
+        listArrayImplementation.set(1, "Samuel");
+        assertFalse(listArrayImplementation.isEmpty());
+        assertEquals("Samuel", listArrayImplementation.get(1));
+    }
+    @Test public void getElementsInTheListAfterRemoving(){
+        listArrayImplementation.add(0, "OOP");
+        listArrayImplementation.add("Dele");
+        listArrayImplementation.add("Ope");
+        listArrayImplementation.remove(1);
+        assertFalse(listArrayImplementation.isEmpty());
+        assertEquals("[OOP, Ope, null, null]", Arrays.toString(listArrayImplementation.getListElement()));
+    }
+    @Test public void getElementsInTheListAfterRemovingWithIndexTest(){
+        listArrayImplementation.add(0, "OOP");
+        listArrayImplementation.add("Dele");
+        listArrayImplementation.add("Ope");
+        listArrayImplementation.remove(0);
+        assertFalse(listArrayImplementation.isEmpty());
+        String[] array = listArrayImplementation.getListElement();
+        assertEquals("[Dele, Ope, null, null]", Arrays.toString(array));
+    }
+    @Test public void getElementsInTheListAfterRemovingWithElementTest(){
+        listArrayImplementation.add(0, "OOP");
+        listArrayImplementation.add("Dele");
+        listArrayImplementation.add("Ope");
+        listArrayImplementation.remove("Dele");
+        assertFalse(listArrayImplementation.isEmpty());
+        String[] array = listArrayImplementation.getListElement();
+        assertEquals("[OOP, Ope, null, null]", Arrays.toString(array));
+    }
+    @Test public void indexOfAnElementCanBeGottenInAList(){
+        listArrayImplementation.add(0, "OOP");
+        listArrayImplementation.add("Dele");
+        listArrayImplementation.add("Ope");
+        assertFalse(listArrayImplementation.isEmpty());
+        assertEquals(1, listArrayImplementation.indexOf("Dele"));
+    }
+    @Test public void indexOfAnElementCanBeGottenInAListTest(){
+        listArrayImplementation.add(0, "OOP");
+        listArrayImplementation.add("Dele");
+        listArrayImplementation.add("Ope");
+        assertFalse(listArrayImplementation.isEmpty());
+        assertEquals(0, listArrayImplementation.indexOf("OOP"));
+    }
+    @Test public void aListCanBeCheckedIfItContainsAnElement(){
+        listArrayImplementation.add(0, "OOP");
+        listArrayImplementation.add("Dele");
+        listArrayImplementation.add("Ope");
+        assertFalse(listArrayImplementation.isEmpty());
+        assertTrue(listArrayImplementation.contains("OOP"));
+    }
+    @Test public void aListCanBeCheckedIfItContainsAnElementTest(){
+        listArrayImplementation.add(0, "OOP");
+        listArrayImplementation.add("Dele");
+        listArrayImplementation.add("Ope");
+        assertFalse(listArrayImplementation.isEmpty());
+        assertFalse(listArrayImplementation.contains("Tayo"));
+    }
+
+}
